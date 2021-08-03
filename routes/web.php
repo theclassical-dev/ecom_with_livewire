49bc617b2d7b8cb332902909b664e4646af3e5de
Abraham Peter <theclassical001@gmail.com>
2021-08-03 22:44:24 +0100

    jetstream installed

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('/', HomeComponent::class);
 Route::get('/cart', CartComponent::class);
 Route::get('/checkout', CheckoutComponent::class);
 
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
